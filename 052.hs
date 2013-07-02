33f32673304d28175c7ec53160ba23e85e8cb47a
seri <seritrinh@gmail.com>
2013-07-02 10:06:57 +0700

    52 PermutedMultiples Easy simulation

--- 052.hs
+++ 052.hs
@@ -5,6 +5,4 @@ isSame xs = xs == replicate (length xs) (head xs)
 
 isCool n = isSame . map (sort . (digits 10) . (* n)) $ [1..6]
 
-search = head . filter isCool . iterate (+3) $ 100002
-
-main = print search 
+main = print . head . filter isCool . iterate (+3) $ 100002
