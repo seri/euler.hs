b1193aa12530404f9a6c8fda4a240103eeb052f7
seri <seritrinh@gmail.com>
2013-07-02 10:04:36 +0700

    53 CombinatoricSelection Short and elegant solution

--- 053.hs
+++ 053.hs
@@ -1,3 +1,3 @@
-pascal = iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1]
+pascal = iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1, 1]
 
-main = print $ length . filter (> 10^6) . concat . take 101 $ pascal
+main = print . length . filter (> 10^6) . concat . take 100 $ pascal
