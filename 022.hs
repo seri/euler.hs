699144928ef37ed73540c63194a4586b57acdfba
seri <seritrinh@gmail.com>
2014-07-07 12:39:54 +0700

    Maintenance

--- 022.hs
+++ 022.hs
@@ -5,5 +5,5 @@ solve names = sum . zipWith score [1..] . sort . read $ "[" ++ names ++ "]"
     where score i name = i * (sum . map alpha $ name)
           alpha c = ord c - ord 'A' + 1
 
-main = print . solve =<< readFile "022.inp"
+main = print . solve =<< readFile "022.in"
 
