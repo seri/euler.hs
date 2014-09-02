26996952ac6af3c9462ab364ca1bec2a347835b6
seri <seritrinh@gmail.com>
2014-09-02 15:50:28 +0700

    59 XORDecryption Elegant solution using frequency analysis

--- 059.hs
+++ 059.hs
@@ -1,5 +1,5 @@
 -- Haskell implementation of the elegant approach described here:
---      http://www.mathblog.dk/project-euler-59-xor-encryption/
+-- http://www.mathblog.dk/project-euler-59-xor-encryption/
 
 import Data.Char
 import Data.Bits
