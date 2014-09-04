a2fec3d6bc184f76be0017784645fc4134ed303f
seri <seritrinh@gmail.com>
2014-09-04 22:34:00 +0700

    63 PowerfulDigitCounts Powerful one-liner

--- 063.hs
+++ 063.hs
@@ -1,2 +1,3 @@
 main :: IO ()
-main = (print . sum . map (floor . flip logBase 0.1 . (/10) . fromIntegral)) [1..9]
+main = (print . sum . map findLimit) [1..9] where
+    findLimit = floor . flip logBase 0.1 . (/10) . fromIntegral
