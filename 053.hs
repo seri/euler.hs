2d0f6ecdd4cba69602a2dc94f1c20c49f332f6c7
seri <seritrinh@gmail.com>
2014-09-09 23:06:22 +0700

    combinatoric selections (elegant with pascal triangle)

--- 053.hs
+++ 053.hs
@@ -1,3 +1,6 @@
-pascal = iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1, 1]
+pascal :: Integral a => [[a]]
+pascal = iterate nextRow [1, 1] where
+    nextRow row = zipWith (+) (0 : row) (row ++ [0])
 
-main = print . length . filter (> 10^6) . concat . take 100 $ pascal
+main :: IO ()
+main = (print . length . filter (> 10 ^ 6) . concat . take 100) pascal
