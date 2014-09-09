c9cec8ce94830cee923a36f8989a2f0a2af65841
seri <seritrinh@gmail.com>
2014-09-09 23:17:11 +0700

    square root convergents (Data.Ratio)

--- 057.hs
+++ 057.hs
@@ -1,11 +1,17 @@
 import Data.Digits
 import Data.Ratio
 
+inverse :: Rational -> Rational
 inverse x = (denominator x) % (numerator x)
 
-series = iterate (\x -> 1 + (inverse (1 + x))) (1 + (1 % 2))
+series :: [Rational]
+series = iterate next (1 + (1 % 2)) where
+    next x = 1 + (inverse (1 + x))
 
-check x = f (numerator x) > f (denominator x)
-    where f n = length . digits 10 $ n
+isCool :: Rational -> Bool
+isCool x = f (numerator x) > f (denominator x) where 
+    f :: Integer -> Int
+    f n = (length . digits 10) n
 
-main = print $ length . filter check . take 1000 $ series
+main :: IO ()
+main = (print . length . filter isCool . take 1000) series
