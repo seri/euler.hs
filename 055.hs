9ece8f8790d27d37039e6bb8dccc34e7ac2ccc56
seri <seritrinh@gmail.com>
2014-09-09 23:11:09 +0700

    lychrel numbers (digits and unDigits)

--- 055.hs
+++ 055.hs
@@ -1,14 +1,17 @@
 import Data.Digits
 import Data.List
 
+limit :: Int
 limit = 10000
 
-isPalin n = ds == reverse ds where ds = digits 10 n
+palindrome :: Int -> Bool
+palindrome n = ds == reverse ds where ds = digits 10 n
 
-backward n = unDigits 10 (reverse ds) where ds = digits 10 n
+compute :: Int -> Int
+compute n = n + unDigits 10 (reverse (digits 10 n))
 
-compute n = n + backward n 
+isLychrel :: Int -> Bool
+isLychrel = not . any palindrome . take 50 . tail . iterate compute
 
-isLychrel = not . any isPalin . take 50 . tail . iterate compute
-
-main = print . length . filter isLychrel $ [1..limit]
+main :: IO ()
+main = (print . length . filter isLychrel) [1 .. limit]
