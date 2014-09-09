b5a5815cb2f5cbd2db490baf486f8efe9ad81ad6
seri <seritrinh@gmail.com>
2014-09-09 21:37:19 +0700

    prime digit replacements (observasions, maybe manip, msum, fast prime sieve)

--- 051.hs
+++ 051.hs
@@ -1,74 +1,62 @@
-{-
-    Some observations:
-    - The last digit can only be 1, 3, 7, 9 so it doesn't belong to the mask.
-    - The mask has a size of 3x. We will cheat and assume that it is indeed 3.
-    - The smallest prime of the family will have a mask of 000, 111, or 222.
-    - We will cheat again and assume that the family has no more than 6 digits.
-    - The last cheat: all remaining digits of the answer != the repeated digit
--}
-
-import Data.List hiding (union)
-import Data.List.Ordered (union, minus)
+import Data.List
+import qualified Data.List.Ordered as OL
+import Data.Maybe
 import Data.Array
 import Data.Digits
-
 import Control.Monad
-import Data.Maybe
 
+allPrimes :: Integral a => [a]
+allPrimes = 2: 3: OL.minus [5, 7..] sieve where
+    sieve = (foldr safeUnion [] . map skipList . tail) allPrimes
+    skipList p = iterate (+ (p + p)) (p * p)
+    safeUnion (x:xs) ys = x : OL.union xs ys
 
--- Prime meme
+domain :: (Int, Int)
+domain = (56993, 999999)
 
-upperBound = 999999
-lowerBound = 56993
+primes :: [Int]
+primes = (takeWhile (<= snd domain) . dropWhile (<= fst domain)) allPrimes
 
-primes = 2: 3: minus [5, 7..] sieve where
-    sieve = foldr safeUnion [] . map skipList . tail $ primes
-    skipList p = iterate (+ (2 * p)) $ p * p
-    safeUnion (x:xs) ys = x: union xs ys
+isPrime :: Int -> Bool
+isPrime = (arr !) where
+    arr = listArray (2, snd domain) (repeat False) // 
+          zip primes (repeat True)
 
-myPrimes = takeWhile (<= upperBound) . dropWhile (<= lowerBound) $ primes
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
 
-primeArr = listArray (2, upperBound) (repeat False) // 
-           zip myPrimes (repeat True)
+replaceAll :: Eq a => a -> [a] -> a -> [a]
+replaceAll x ys z = map (f x z) ys where
+    f x z y = if y == x then z else y
 
-isPrime n = primeArr ! n
+findMaskDigit :: [Int] -> Maybe Int
+findMaskDigit xs = (msum . map (try xs)) [0, 1, 2] where 
+    try xs x = if count x xs == 3 then Just x
+                                  else Nothing
 
+findMask :: [Int] -> Maybe [Int]
+findMask xs = if isJust d then Just (replaceAll (fromJust d) xs (-1))
+                          else Nothing
+    where d = findMaskDigit xs
 
--- Some list utils
+generateFamily :: Int -> [Int]
+generateFamily p = if isJust mask then result else [] where 
+    mask = (findMask . (digits 10)) p
+    result = map ((unDigits 10) . (replaceAll (-1) (fromJust mask))) [0..9]
 
-count x [] = 0
-count x (y:xs) = if x == y then 1 + count x xs
-                           else count x xs
-
-replaceAll p [] q = []
-replaceAll p (x:xs) q = if x == p then q : replaceAll p xs q
-                                  else x : replaceAll p xs q
-
-
--- Transform a list of digits into masked form
-
-findMaskDigit xs = msum . map (try xs) $ [0, 1, 2] 
-    where try xs x = if count x xs == 3 then Just x
-                                        else Nothing
-
-findMask xs = if isJust temp then Just $ replaceAll (fromJust temp) xs (-1)
-                             else Nothing
-    where temp = findMaskDigit xs
-
-
--- Given the smallest member, generate all members of the family
-
-generateFamily p =
-    if isJust temp then map ((unDigits 10) . (replaceAll (-1) (fromJust temp))) [0..9]
-                   else []
-    where temp = (findMask . (digits 10)) p
-
+isFamilyCool :: [Int] -> Bool
 isFamilyCool ps = count True (map isPrime ps) == 8
 
- 
+main :: IO ()
+main = (print . msum . map try) primes where
+    try p = if isFamilyCool (generateFamily p) then Just p
+                                               else Nothing
 
-search = msum . map try $ myPrimes
-    where try p = if isFamilyCool (generateFamily p) then Just p
-                                                     else Nothing
-
-main = print search
+{-
+    Some observations:
+    - The last digit can only be 1, 3, 7, 9 so it doesn't belong to the mask
+    - The mask has a size of 3x. We will cheat and assume that it is indeed 3
+    - The smallest prime of the family will have a mask of 000, 111, or 222
+    - We will cheat again and assume that the family has no more than 6 digits
+    - The last cheat: all remaining digits of the answer != the repeated digit
+-}
