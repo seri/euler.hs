83b64904a594909db3592a12255e93a3175c43c0
seri <seritrinh@gmail.com>
2014-09-11 00:47:21 +0700

    convergents of e (simple simulation)

--- 065.hs
+++ 065.hs
@@ -1,16 +1,16 @@
 import Data.Digits
 import Data.Ratio
 
-type Fancy = (Integer, [Integer])
+type Irrational = (Integer, [Integer])
 
-(%!) :: Fancy -> Int -> Rational
-(x, xs) %! n = x % 1 + 1 / foldr f 0 (take (n - 1) xs) where
+approx :: Irrational -> Int -> Rational
+approx (x, xs) n = x % 1 + 1 / foldr f 0 (take (n - 1) xs) where
     f x 0 = x % 1
     f x r = x % 1 + (1 / r)
 
-fancyE :: Fancy
+fancyE :: Irrational
 fancyE = (2, concatMap surround (iterate (+ 2) 2)) where
     surround x = [1, x, 1]
 
 main :: IO ()
-main = (print . sum . digits 10 . numerator) (fancyE %! 100 )
+main = (print . sum . digits 10 . numerator) (fancyE `approx` 100 )
